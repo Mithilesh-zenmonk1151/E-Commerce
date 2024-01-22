055ed582f3dfd9de98b955c3621e814a5ccdb1f2
Mithilesh-zenmonk1151 <mithilesh.1151@zenmonk.tech>
2024-01-22 18:01:05 +0530

    intial commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+function Home() {
+    return (
+       <div className="home">
+        <div className="hero">
+            <div className="hero-left">
+                <div className="women-fashion"> <p>Women's Fashion</p></div>
+               <div className="men-fashion">
+               <p>Men's Fashion</p>
+               </div>
+               
+                <p>Electronics</p>
+                <p>Home & Lifestyle</p>
+                <p>Medicine</p>
+                <p>Sports & Outdoor</p>
+                <p>Baby's & Toys</p>
+                <p>Groceries & Pets</p>
+                <p>Health & Beauty</p>
+
+                
+            </div>
+            <div className="middle-hero">
+
+            </div>
+        </div>
+       </div>
+    )
+}
+
+export default Home
